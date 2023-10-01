eff0d82bc0caa971b5c270870f642f51921a9a41
Аnastasia Baybikova <52564341+trigbayb@users.noreply.github.com>
2023-10-01 22:43:05 +0300

    Time: 978 ms (83.07%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -3,7 +3,7 @@ select distinct num as ConsecutiveNums
 from  
 (select num, 
         lag(num) over (order by id) as lags, 
-        lead(num) over (order by id) as leads
+        lead(num) over (order by id) as leads 
 from Logs)A
 where num=lags
 and num=leads
